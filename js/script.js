a5ef024fc01fa23ac2fe26b3c0d674307395210c
Jamie Byram <jamie.byram@gmail.com>
2018-02-17 21:12:13 +0000

    Add score tracking

--- js/script.js
+++ js/script.js
@@ -1,41 +1,30 @@
-// Define global variables
+// Global variables
 
-let userPoints = 0;
-let computerPoints = 0;
-let round = 1;
+let playerScore = 0;
+let computerScore = 0;
 const results = document.querySelector('#results');
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
 
 // Player selection
 
-let rock = document.querySelector("#rock");
-let paper = document.querySelector("#paper");
-let scissors = document.querySelector("#scissors");
-
-rock.addEventListener("click", function(e){
-  let playerSelection = "rock";
-  let computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
-  const rockResult = document.createElement('p');
-  rockResult.textContent = (playRound(playerSelection, computerSelection));
-  results.appendChild(rockResult);
+rock.addEventListener("click", function(){
+  document.getElementById("result").innerHTML = (playRound("rock", computerPlay()));
+  document.getElementById("playerscore").innerHTML = playerScore;
+  document.getElementById("computerscore").innerHTML = computerScore;
 })
 
-paper.addEventListener("click", function(e){
-  let playerSelection = "paper";
-  let computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
-  const paperResult = document.createElement('p');
-  paperResult.textContent = (playRound(playerSelection, computerSelection));
-  results.appendChild(paperResult);
+paper.addEventListener("click", function(){
+  document.getElementById("result").innerHTML = (playRound("paper", computerPlay()));
+  document.getElementById("playerscore").innerHTML = playerScore;
+  document.getElementById("computerscore").innerHTML = computerScore;
 })
 
 scissors.addEventListener("click", function(e){
-  let playerSelection = "scissors";
-  let computerSelection = computerPlay();
-  console.log(playRound(playerSelection, computerSelection));
-  const scissorsResult = document.createElement('p');
-  scissorsResult.textContent = (playRound(playerSelection, computerSelection));
-  results.appendChild(scissorsResult);
+  document.getElementById("result").innerHTML = (playRound("scissors", computerPlay()));
+  document.getElementById("playerscore").innerHTML = playerScore;
+  document.getElementById("computerscore").innerHTML = computerScore;
 })
   
 // Randomly generate the computer's play 
@@ -52,56 +41,44 @@ function computerPlay() {
 }
 
 // Compares the player's selection with the computer's selection, increments the points accordingly, 
-// then returns a string with the result back to the calling function. If the player cancels their
-// turn, or makes an invalid entry, the computer wins the round
+// then returns a string with the result back to the calling function.
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == null) {
-    computerPoints++;
-    return "You cancelled your turn. Computer wins the round!";
-  } else if ((playerSelection.toUpperCase() == "ROCK") && (computerSelection == "paper")) {
-    computerPoints++;
+  if ((playerSelection == "rock") && (computerSelection == "paper")) {
+    computerScore++;
     return "You played rock. Computer played paper. Computer wins the round!";
-  } else if ((playerSelection.toUpperCase() == "PAPER") && (computerSelection == "rock")) {
-    userPoints++;
+  } else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+    playerScore++;
     return "You played paper. Computer played rock. You win the round!";
-  } else if ((playerSelection.toUpperCase() == "SCISSORS") && (computerSelection == "rock")) {
-    computerPoints++;
+  } else if ((playerSelection == "scissors") && (computerSelection == "rock")) {
+    computerScore++;
     return "You played scissors. Computer played rock. Computer wins the round!";
-  } else if ((playerSelection.toUpperCase() == "ROCK") && (computerSelection == "scissors")) {
-    userPoints++;
+  } else if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+    playerScore++;
     return "You played rock. Computer played scissors. You win the round!";
-  } else if ((playerSelection.toUpperCase() == "PAPER") && (computerSelection == "scissors")) {
-    computerPoints++;
+  } else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+    computerScore++;
     return "You played paper. Computer played scissors. Computer wins the round!";
-  } else if ((playerSelection.toUpperCase() == "SCISSORS") && (computerSelection == "paper")) {
-    userPoints++;
+  } else if ((playerSelection == "scissors") && (computerSelection == "paper")) {
+    playerScore++;
     return "You played scissors. Computer played paper. You win the round!";
-  } else if ((playerSelection.toUpperCase() == "ROCK") && (computerSelection == "rock")) {
+  } else if ((playerSelection == "rock") && (computerSelection == "rock")) {
     return "You played rock. Computer played rock. Round is a tie!";
-  } else if ((playerSelection.toUpperCase() == "PAPER") && (computerSelection == "paper")) {
+  } else if ((playerSelection == "paper") && (computerSelection == "paper")) {
     return "You played paper. Computer played paper. Round is a tie!";
-  } else if ((playerSelection.toUpperCase() == "SCISSORS") && (computerSelection == "scissors")) {
+  } else if ((playerSelection == "scissors") && (computerSelection == "scissors")) {
     return "You played scissors. Computer played scissors. Round is a tie!";
-  } else {
-    computerPoints++;
-    return "Sorry, your entry was not valid. Computer wins the round!";
   }
 }
 
 // Prints the user's points, the computer's points, and the result of the game
   
-function endReport() {
-  console.log("You scored " + userPoints);
-  console.log("Computer scored " + computerPoints);
-  if (userPoints < computerPoints) {
+function announceWinner() {
+  if (playerScore < computerScore) {
     console.log("Computer wins!");
-  } else if (userPoints > computerPoints) {
+  } else if (playerScore > computerScore) {
     console.log("You win!");
   } else console.log("Game tied!");
 }
 
-// Starts the game
-
-// game();
 
